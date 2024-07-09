8383da6673b42d80e476615a5da0db55e6bc1f26
HCK04 <meganx6s@gmail.com>
2024-07-09 14:16:00 +0100

    Laravel Sessions

---
+++ first/resources/views/test.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <h1>Hello <?php echo $nom?></h1>
+    
+</body>
+</html>
+
